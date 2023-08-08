d59f8886c57753c50f717d389db611085a243d7b
Alex Villablanca <alex@villablanca.tech>
2023-08-07 19:06:30 -0700

    added sentry support

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,11 +2,15 @@ import { defineConfig } from 'vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
 import { qwikCity } from '@builder.io/qwik-city/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { match } from 'assert';
 
 export default defineConfig(async () => {
   const { default: rehypePrettyCode } = await import("rehype-pretty-code");
   return {
+    build: {
+        sourcemap: true,
+    },
     plugins: [qwikCity({
       mdxPlugins: {
         remarkGfm: true,
@@ -26,7 +30,12 @@ export default defineConfig(async () => {
         ],
       },
       trailingSlash: false,
-    }), qwikVite(), tsconfigPaths()],
+    }), qwikVite(), tsconfigPaths(), sentryVitePlugin({
+    authToken: process.env.SENTRY_AUTH_TOKEN,
+    org: "sentry",
+    project: "leaguetracker",
+    url: "https://debug.villablanca.tech",
+    })],
     preview: {
       headers: {
         'Cache-Control': 'public, max-age=600',
